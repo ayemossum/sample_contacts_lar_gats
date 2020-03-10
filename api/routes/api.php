ebc3de67c432d1b841e517c82e31bde2edd1f5f6
ayemossum <ayemossum@gmail.com>
2020-03-10 11:13:00 -0700

    backend

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Contact;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,63 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/contacts', function () {
+    return Contact::all();
+});
+
+Route::get('/contacts/{id}', function ($id) {
+    return Contact::findOrFail($id);;
+});
+
+Route::put('/contacts/{id}', function (Request $request, $id) {
+    $contact = Contact::findOrFail($id);
+    $parameters = $request->json();
+
+    if (!$parameters->get("firstname")) {
+        return response("First Name is required", 406);
+    }
+    if (!$parameters->get("lastname")) {
+        return response("Last Name is required", 406);
+    }
+    if (!$parameters->get("email")) {
+        return response("Email is required", 406);
+    }
+
+    $contact->firstname = $parameters->get("firstname");
+    $contact->lastname = $parameters->get("lastname");
+    $contact->email = $parameters->get("email");
+    $contact->save();
+
+    return [ "message" => "OK" ];
+});
+
+Route::post('/contacts', function (Request $request) {
+    $parameters = $request->json();
+    if ($parameters->get("id")) {
+        return response("Invalid post, id given", 409);
+    }
+    if (!$parameters->get("firstname")) {
+        return response("First Name is required", 406);
+    }
+    if (!$parameters->get("lastname")) {
+        return response("Last Name is required", 406);
+    }
+    if (!$parameters->get("email")) {
+        return response("Email is required", 406);
+    }
+    $contact = new Contact;
+    $contact->firstname = $parameters->get("firstname");
+    $contact->lastname = $parameters->get("lastname");
+    $contact->email = $parameters->get("email");
+    $contact->save();
+
+    return [ "id" => $contact->id ];
+});
+
+Route::delete("/contacts/{id}", function ($id) {
+    $contact = Contact::findOrFail($id);
+    $contact->delete();
+    return [ "message" => "Ok" ];
 });
